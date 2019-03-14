119b44060a0184e90baeb61e9ba34d9d15c8492d
Dmytro_Polianskyi <Dmytro_Polianskyi@epam.com>
2019-03-14 14:57:37 +0200

    commit 2 to 10

---
+++ src/main/java/com.epam.addressbook/AccommodationRepository.java
@@ -0,0 +1,16 @@
+package com.epam.addressbook;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AccommodationRepository {
+    Optional<Accommodation> create(Accommodation accommodation);
+
+    Optional<Accommodation> getById(Long id);
+
+    Optional<List<Accommodation>> findAll();
+
+    Optional<Accommodation> update(Long id, Accommodation accommodation);
+
+    void delete(Long id);
+}
---
+++ src/main/java/com.epam.addressbook/AddressBookApplication.java
@@ -0,0 +1,32 @@
+package com.epam.addressbook;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
+
+@SpringBootApplication
+public class AddressBookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookApplication.class, args);
+    }
+
+    @Bean
+    AccommodationRepository accommodationRepository() {
+        return new InMemoryAccommodationRepository();
+    }
+
+    @Bean
+    public ObjectMapper jsonObjectMapper() {
+        return Jackson2ObjectMapperBuilder.json()
+                .serializationInclusion(NON_NULL)
+                .featuresToDisable(WRITE_DATES_AS_TIMESTAMPS)
+                .modules(new JavaTimeModule())
+                .build();
+    }
+}
